c97ebbce6ef687b9cbb7b76473dd82bc8874112d
andre <andreamusholin@gmail.com>
2024-11-27 19:31:48 +0700

    last

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Fu1xS45r7HWerDjn9XZNbUqMqC8UbL8Q7y7D8gwFySc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
